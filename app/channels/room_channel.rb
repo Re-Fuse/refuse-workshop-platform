1ceaa731dbd4144f37c03f367e0309c256777e40
Kieran Klaassen <kieranklaassen@gmail.com>
2017-03-18 16:07:25 -0500

    Set names in chat

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -8,6 +8,6 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    Message.create! value: data['message'], user_id: 1, room_id: 1
+    Message.create! value: data['message'], user: current_user, room_id: 1
   end
 end
