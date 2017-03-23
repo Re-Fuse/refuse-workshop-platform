43b187fb9f8efeefe2c29397241227eb2a9471ff
Kieran Klaassen <kieranklaassen@gmail.com>
2017-03-23 10:04:53 -0500

    Fix cue activation

--- app/jobs/activate_cue_broadcast_job.rb
+++ app/jobs/activate_cue_broadcast_job.rb
@@ -2,6 +2,6 @@ class ActivateCueBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(cue)
-    ActionCable.server.broadcast channel, message: cue.render, id: cue.id, room_id: cue.room_id
+    ActionCable.server.broadcast 'cues', message: cue.render, id: cue.id, room_id: cue.room_id
   end
 end
